2d3bb6917f8c6de34b1856697d08519b410116c3
Annie Kwan <akwan.mtl@gmail.com>
2021-03-08 19:17:49 -0500

    added bulma with template from https://medium.com/@thexap/how-to-setup-bulma-css-framework-with-react-under-5-minutes-a3d8c2c33a87

---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg ">
+        
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
