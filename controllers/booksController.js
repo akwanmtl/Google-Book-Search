ccc1d003e176b9db0dbc3eb8a3d0c8ca48e50836
Annie Kwan <akwan.mtl@gmail.com>
2021-03-10 09:40:31 -0500

    added seeder file and model for books

---
+++ controllers/booksController.js
@@ -0,0 +1,30 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(result => res.json(result))
+      .catch(err => res.status(422).json(err));
+  }
+};
