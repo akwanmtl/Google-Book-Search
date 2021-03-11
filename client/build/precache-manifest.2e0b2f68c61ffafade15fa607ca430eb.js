e69ead9bf0bf8a9fb7a7001d08abea0f3739a4a9
Annie Kwan <akwan.mtl@gmail.com>
2021-03-11 09:45:08 -0500

    build client folder

---
+++ client/build/precache-manifest.2e0b2f68c61ffafade15fa607ca430eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2979e1a4f6af23ec9994",
+    "url": "/static/css/main.998168e9.chunk.css"
+  },
+  {
+    "revision": "2979e1a4f6af23ec9994",
+    "url": "/static/js/main.41bd4e91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "503ac19ab71a8e4053c4",
+    "url": "/static/js/2.3bd42cd3.chunk.js"
+  },
+  {
+    "revision": "ccc622eb3de5c142b48bd69e0b2c9b30",
+    "url": "/static/media/pexels-clem-onojeghuo-185764.ccc622eb.jpg"
+  },
+  {
+    "revision": "3fb0b00349572862bffaea67a26605f6",
+    "url": "/index.html"
+  }
+];
